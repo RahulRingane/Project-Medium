b9d3ba8ef3372cb11b1b442b475858f5fa96942d
rahul ringane <rahul897r@gmail.com>
2024-07-29 22:31:39 +0530

    updated files with specific changes

--- Project-Medium/frontend/src/components/Appbar.tsx
+++ Project-Medium/frontend/src/components/Appbar.tsx
@@ -17,7 +17,7 @@ export const Appbar = () => {
                 </div>
                 <div className="flex justify-center flex-col ml-3">
                     <Link to={`/publish`}>
-                        <button type="button" className=" ml-1  md:ml-4 md:mr-3 text-white bg-green-600 hover:bg-green-700 focus:outline-none 
+                        <button type="button" className=" md:ml-4 md:mr-4 text-white bg-green-600 hover:bg-green-700 focus:outline-none 
             focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 md:py-2.5 text-center
              me-2 hover:bg-green-700">Publish</button>
                     </Link>
